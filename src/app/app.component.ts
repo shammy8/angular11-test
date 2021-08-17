9cb7486ff8b135aa236e9540b1ebb47e4a66756c
allansham <allan.sham@trapezegroup.com>
2021-08-17 15:58:36 +0100

    Install webix, include them and create a pivot table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as webix from '@xbs/webix-pro'; // un-commenting this throws the unknown view: pivot error
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+    <div id="webix-button"></div>
+    <div id="pivot-table" style="height: 600px"></div>
   `,
-  styles: []
+  styles: [],
 })
-export class AppComponent {
-  title = 'angular11-test';
+export class AppComponent implements OnInit {
+  button!: webix.ui.button;
+  pivotTable!: webix.ui.pivot;
+
+  ngOnInit() {
+    this.button = webix.ui({
+      container: 'webix-button',
+      view: 'button',
+      label: 'Test',
+    }) as webix.ui.button;
+
+    this.pivotTable = webix.ui({
+      container: 'pivot-table',
+      view: 'pivot',
+      max: 'true',
+      data: [
+        {
+          name: 'Argentina',
+          year: 2005,
+          continent: 'South America',
+          form: 'Republic',
+          gdp: 181.357,
+          oil: 1.545,
+          balance: 4.699,
+        },
+        {
+          name: 'Argentina',
+          year: 2006,
+          continent: 'South America',
+          form: 'Republic',
+          gdp: 212.507,
+          oil: 1.732,
+          balance: 7.167,
+        },
+      ],
+      structure: {
+        rows: ['form', 'name'],
+        columns: ['year'],
+        values: [
+          { name: 'gdp', operation: 'sum' },
+          { name: 'oil', operation: 'sum' },
+        ],
+        filters: [],
+      },
+    }) as webix.ui.pivot;
+  }
 }
